ee35d5c6dcb47574167f4fc5a6417e88d1316c09
baozoudashijian <Yhldcahcy521>
2021-12-14 16:05:58 +0800

    "能成功运行的版本"

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  watch = require('gulp-watch'),
+  browser = require('browser-sync'),
+  imagemin = require('gulp-imagemin');
+
+var path = {
+    sass: 'src/sass/*.scss',
+    js: 'src/js/*.js',
+    html: 'src/*.html',
+    image: 'src/image/*.{png,jpg,gif,ico}'
+}
+
+// 编译sass
+gulp.task('sass', function(){
+    gulp.src(path.sass)
+      .pipe(sass())
+      .pipe(gulp.dest('./build/css/'));
+});
+
+// 编译js
+gulp.task('js', function(){
+    gulp.src(path.js)
+      .pipe(gulp.dest('./build/js/'));
+});
+
+// 编译html
+gulp.task('html', function(){
+    gulp.src(path.html)
+      .pipe(gulp.dest('./build/'));
+});
+// 压缩图片
+gulp.task('image', function(){
+    gulp.src(path.image)
+      .pipe(gulp.dest('./build/image/'));
+});
+
+// 自动刷新 检测src || build
+var DEV = 'build';
+gulp.task('serve', function() {
+    browser.init({
+        server: DEV + '/'
+    });
+    gulp.watch(DEV + '/*.html' ).on('change', browser.reload)
+    gulp.watch(DEV + '/js/*.js').on('change', browser.reload)
+    gulp.watch(DEV + '/sass/*.scss').on('change', browser.reload)
+    gulp.watch(DEV + '/image/*.{png,jpg,gif,ico}').on('change', browser.reload)
+});
+
+gulp.task('watch', function(){
+    gulp.watch(path.sass, ['sass']);
+    gulp.watch(path.js, ['js']);
+    gulp.watch(path.html, ['html']);
+    gulp.watch(path.image, ['image']);
+});
+
+gulp.task('default', ['watch', 'serve', 'sass', 'js', 'html', 'image']);
---
+++ readme.md
@@ -0,0 +1,16 @@
+## 环境
+node: 11.15.0
+
+gulp: 3.9.1
+
+gulp-cli: 务必与gulp保持一致
+
+## 使用教程：
+
+[demo](http://kang588.oschina.io/webpack/src)
+
+1、首次运行先全局安装gulp 命令行中执行：yarn add global gulp
+
+2、yarn安装依赖
+
+3、再运行yarn start
